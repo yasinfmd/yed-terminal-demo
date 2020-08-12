af121908f8621c6219c74074e354a6dc1e502aca
Yasin Dalkılıç <39165806+yasinfmd@users.noreply.github.com>
2020-08-12 12:55:06 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
   return (
       <>
           <p>Başlık ile beraber kullanım</p>
-        <YedTerminal terminalTitle={"Terminalim"} terminalLogs={[{label:'Merhaba'},{label: "Yasin Efem Dalkılıç"}]} />
+        <YedTerminal terminalTitle={"Terminalim"} terminalType="Mac" terminalLogs={[{label:'Merhaba'},{label: "Yasin Efem Dalkılıç"}]} />
+    
+       <p> Windows için  kullanım</p>
+        <YedTerminal terminalTitle={"Terminalim"} terminalType="Windows" terminalLogs={[{label:'Merhaba'},{label: "Yasin Efem Dalkılıç"}]} />
 
           <p>Farklı yükseklik değerleri ile kullanım</p>
           <YedTerminal terminalHeight={300} terminalTitle={"Terminalim"} terminalLogs={[{label:'Merhaba'},{label: "Yasin Efem Dalkılıç"}]} />
